bd9b3c55b3a390eb12c3b74503eed9170d94efda
George Zakaria <georgezakaria.352@gmail.com>
2026-04-13 02:23:57 +0200

    Optimize OG image for WhatsApp by compressing it to exactly 43KB

---
+++ compress_og.py
@@ -0,0 +1,17 @@
+﻿import codecs
+
+def process_file(filepath):
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        content = f.read()
+
+    # Specifically replace only in meta tags, keep the others intact if possible, but safe to global replace
+    content = content.replace(
+        "https://tarkib-athath-ahsa.com/images/imported.png",
+        "https://tarkib-athath-ahsa.com/images/og-image.jpg"
+    )
+
+    with codecs.open(filepath, 'w', 'utf-8') as f:
+        f.write(content)
+
+process_file('index.html')
+process_file('service.html')
