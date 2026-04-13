ad7aecb41cb5a769bdb13d7ce8254a73019f099c
George Zakaria <georgezakaria.352@gmail.com>
2026-04-13 02:32:39 +0200

    Shorten og:title to allow WhatsApp to display og:description

---
+++ shorten_og.py
@@ -0,0 +1,17 @@
+﻿import codecs
+
+def process_file(filepath):
+    with codecs.open(filepath, 'r', 'utf-8') as f:
+        content = f.read()
+
+    # We only change the og:title (social media) and NOT the main <title> tag.
+    content = content.replace(
+        '<meta property="og:title" content="فني تركيب أثاث وايكيا بالأحساء - 0574426629 اتصل الآن">',
+        '<meta property="og:title" content="أبو إسلام لتركيب الأثاث بالأحساء">'
+    )
+
+    with codecs.open(filepath, 'w', 'utf-8') as f:
+        f.write(content)
+
+process_file('index.html')
+process_file('service.html')
